5e57487d1afde5d8e193dff47154e63b7e607523
Jarred Kenny <jarred.kenny@screenscape.com>
2019-04-11 16:55:01 -0300

    added pip packages

--- .env/node/bootstrap.sh
+++ .env/node/bootstrap.sh
@@ -9,4 +9,6 @@ curl -L https://git.io/n-install | N_PREFIX=$HOME/.tools/n bash -s -- -y -n
 npm install -g npm yarn
 yarn config set prefix $HOME/.tools/yarn
 
-yarn global add diff-so-fancy
+YARN_PACKAGES=$(cat $DIR/../packages-yarn | sed 's/\\n/ /')
+yarn global add $YARN_PACKAGES
+
