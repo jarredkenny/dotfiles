a348088c43856483ef775da480f04cde1b021b65
Jarred Kenny <jarred.kenny@screenscape.com>
2020-04-02 08:21:37 -0300

    Added python/miniconda to tools

---
+++ .env/tools/python/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo "> Bootstraping Python"
+
+wget -O $DIR/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+chmod +x $DIR/miniconda.sh
+
+$DIR/miniconda.sh -b -p $HOME/.tools/miniconda -u
+
+conda config --add channels conda-forge 
+
+PIP_PACKAGES=$(cat $DIR/../packages-pip | sed 's/\\n/ /')
+pip install $PIP_PACKAGES
