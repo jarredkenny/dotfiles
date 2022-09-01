7e60171fa833c8a3673e8c31c7152cd3b299967f
Jarred Kenny <jarred.kenny@libertymutual.com>
2022-09-01 13:16:36 -0300

    updates

--- .env/tools/nim/env.zsh
+++ .env/tools/nim/env.zsh
@@ -1,3 +1,2 @@
-export PATH=$HOME/.nimble/bin:$HOME/.tools/nimble/bin:$PATH
+export PATH=$HOME/.env/tools/nim/Nim/bin:$PATH
 
-alias choosenim="$HOME/.tools/bin/choosenim --choosenimDir:$HOME/.tools/choosenim --nimbleDir:$HOME/.tools/nimble"
